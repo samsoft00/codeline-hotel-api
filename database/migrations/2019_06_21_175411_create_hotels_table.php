aca43294af33d2356f9444252fa700f620bade11
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2019-06-21 18:58:05 +0100

    add hotel model and migration

---
+++ database/migrations/2019_06_21_175411_create_hotels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('zipcode');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
