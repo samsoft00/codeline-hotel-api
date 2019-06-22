19a48bf42c73cb820c917cd02be53c7063b21132
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2019-06-22 08:49:38 +0100

    add controller and update route

---
+++ app/Http/Controllers/CodeLineHotelController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CodeLineHotelController extends Controller
+{
+    public function index(){}
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', 'CodeLineHotelController@index')->where('any', '.*');
